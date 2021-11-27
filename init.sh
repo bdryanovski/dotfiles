df7bacac030f5b28b8f4a226aa45e79880e31329
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2021-11-27 23:41:34 +0200

    feat(osx): update the package and introduce new arguments like help and version
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

--- init.sh
+++ init.sh
@@ -30,13 +30,43 @@ function banner() {
 
 banner;
 
-while true; do
-  read -p "🤌  Do you wish to install dotfiles? [yn]: " yn
-  case $yn in
-    [Yy]* ) start; exit; break;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer yes or no.";;
-  esac
-done
+if test -z "$1"; then
+
+  while true; do
+    read -p "🤌  Do you wish to install dotfiles? [yn]: " yn
+    case $yn in
+      [Yy]* ) start; exit; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+
+else
+
+  if [ "$1" == "--help" ]; then
+    echo "Need to write help"
+  fi
+
+  if [ "$1" == "--packages" ]; then
+
+    infod "Packages: "
+    for i in $(ls -d packages/*); do 
+      if test -f "${i%%/}/init.sh"; then
+        echo "  ${i%%/}"
+      fi
+    done
+
+    echo "  "
+
+    packagedone "Above is the list of supported packages"
+    exit
+  fi
+
+  if test -f "packages/$1/init.sh"; then
+    bash "packages/$1/init.sh" $2
+  fi
+fi
+
+
 
 
