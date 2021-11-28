96f9893b36a2103f915992699b8cb675d1573d9f
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2021-11-28 10:05:33 +0200

    feat: implement help and version arguments to packages
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- packages/git/init.sh
+++ packages/git/init.sh
@@ -2,6 +2,7 @@
 
 source ./helpers.sh
 
+VERSION="1.0.0"
 
 function banner() {
   echo "   _____ _____ _______ "
@@ -22,36 +23,67 @@ package="$PWD/$(dirname "$0")"
 gitconfig=$HOME/.gitconfig
 gitconfigbackup=$HOME/.gitconfig.backup
 
-if commandExist $gitbin; then
-  checked "git is already installed on this system"
-else
-  missed "git command is not found - trying to install it"
-fi
+function setup() {
+
+  if commandExist $gitbin; then
+    checked "git is already installed on this system"
+  else
+    missed "git command is not found - trying to install it"
+  fi
 
 
-if fileExist $gitconfig; then
-  checked "git configuration file exist already - creating backup $gitconfigbackup"
-  cp -f "$gitconfig" "$gitconfigbackup"
-fi
+  if fileExist $gitconfig; then
+    checked "git configuration file exist already - creating backup $gitconfigbackup"
+    cp -f "$gitconfig" "$gitconfigbackup"
+  fi
 
 
-cp "$package/files/gitconfig" "$gitconfig"
-checked "git configuration file created: $gitconfig"
+  cp "$package/files/gitconfig" "$gitconfig"
+  checked "git configuration file created: $gitconfig"
 
 
-if commandExist 'npm'; then
+  if commandExist 'npm'; then
+    helpblock
+    helpline "Node & NPM are not installed, yet"
+    helpline "Internal diff tool must be installed"
+    helpline "npm install -g diff-so-fancy"
+    helpend
+  fi
+
   helpblock
-  helpline "Node & NPM are not installed, yet"
-  helpline "Internal diff tool must be installed"
-  helpline "npm install -g diff-so-fancy"
+  helpline "GPG need manual work so it could run"
+  helpline "Visit this page: https://gpgtools.org"
+  helpline "Install the package and get the SIGNKEY"
+  helpline "Setup the signkey into the $gitconfig so we could automatically sign them"
   helpend
+
+  packagedone "Git is ready to be used"
+}
+
+function help() {
+  helptext " "
+  helptext "Description:"
+  helptext "Configure control version tooling"
+  helptext "This include git configuration, additional commands"
+  helptext "Also instructions how to setup GPG Signing"
+  helptext " "
+}
+
+function version() {
+  helptext "Package version: $VERSION"
+}
+
+
+if [ "$1" == "--help" ]; then
+  help
+  exit;
 fi
 
-helpblock
-helpline "GPG need manual work so it could run"
-helpline "Visit this page: https://gpgtools.org"
-helpline "Install the package and get the SIGNKEY"
-helpline "Setup the signkey into the $gitconfig so we could automatically sign them"
-helpend
+if [ "$1" == "--version" ]; then
+  version
+  exit;
+fi
 
-packagedone "Git is ready to be used"
+setup
+
+
--- packages/vim/init.sh
+++ packages/vim/init.sh
@@ -2,16 +2,18 @@
 
 source ./helpers.sh
 
+VERSION="1.0.0"
+
 function banner() {
-echo " __      _______ __  __ " 
-echo " \ \    / /_   _|  \/  |"
-echo "  \ \  / /  | | | \  / |"
-echo "   \ \/ /   | | | |\/| |"
-echo "    \  /   _| |_| |  | |"
-echo "     \/   |_____|_|  |_|"
-echo "  "
-echo " -- Setup editor NeoVim and Vim "
-echo "  "
+  echo " __      _______ __  __ " 
+  echo " \ \    / /_   _|  \/  |"
+  echo "  \ \  / /  | | | \  / |"
+  echo "   \ \/ /   | | | |\/| |"
+  echo "    \  /   _| |_| |  | |"
+  echo "     \/   |_____|_|  |_|"
+  echo "  "
+  echo " -- Setup editor NeoVim and Vim "
+  echo "  "
 }
 
 banner
@@ -23,52 +25,78 @@ nvmbin="nvm"
 nodeVersion="16.13.0"
 nvimconfigdir="$HOME/.config/nvim"
 
-if commandExist $nvimbin; then
-  checked "NeoVim is installed"
-else
+function setup() {
 
-  if ! commandExist $brewbin; then
-    missing "brew is missing need to install it before that"
-    exit;
+  if commandExist $nvimbin; then
+    checked "NeoVim is installed"
+  else
+
+    if ! commandExist $brewbin; then
+      missing "brew is missing need to install it before that"
+      exit;
+    fi
+
+    brew install tree-sitter luajit neovim
+
+    checked "Installed NeoVim, Tree Sitter and Lua support"
+
+    python3 -m pip install pynvim --user
+
+    checked "Setup Python3 to work with NeoVim"
   fi
 
-  brew install tree-sitter luajit neovim
+  if commandExist $vimbin; then
+    checked "Vim is installed"
+  else
+    missing "Vim is not installed but I prefer nvim so this is on you/me"
+  fi
 
-  checked "Installed NeoVim, Tree Sitter and Lua support"
 
-  python3 -m pip install pynvim --user
+  if fileExist "$nvimconfigdir/init.vim"; then
+    checked "NeoVim is already setup - backuping it before continue"
+    cp -R "$nvimconfigdir" "$nvimconfigdir.backup"
+  fi
 
-  checked "Setup Python3 to work with NeoVim"
+  if ! commandExist $nvmbin; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+    checked "NVM installed"
+  fi
+
+  nvm install $nodeVersion
+
+  npm install -g typescript typescript-language-server diagnostic-languageserver
+
+  brew install exa
+
+  packagedone "Editors are ready to be used."
+}
+
+function help() {
+  helptext " "
+  helptext "Description:"
+  helptext "Setup NeoVim and Vim as editor"
+  helptext "Configure the editors for development by installing additional packages"
+  helptext " "
+}
+
+function version() {
+  helptext "Package version: $VERSION"
+}
+
+
+if [ "$1" == "--help" ]; then
+  help
+  exit;
 fi
 
-if commandExist $vimbin; then
-  checked "Vim is installed"
-else
-  missing "Vim is not installed but I prefer nvim so this is on you/me"
+if [ "$1" == "--version" ]; then
+  version
+  exit;
 fi
 
-
-if fileExist "$nvimconfigdir/init.vim"; then
-  checked "NeoVim is already setup - backuping it before continue"
-  cp -R "$nvimconfigdir" "$nvimconfigdir.backup"
-fi
-
-if ! commandExist $nvmbin; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-  checked "NVM installed"
-fi
-
-nvm install $nodeVersion
-
-npm install -g typescript typescript-language-server diagnostic-languageserver
-
-brew install exa
-
-packagedone "Editors are ready to be used."
-
-
+setup
