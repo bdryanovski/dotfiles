cfffef96f8ee01502e36dfa9be06d5eb2850a88f
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2021-11-28 10:05:33 +0200

    feat: implement help and version arguments to packages
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

--- packages/shell/init.sh
+++ packages/shell/init.sh
@@ -2,6 +2,8 @@
 
 source ./helpers.sh
 
+VERSION="1.0.0"
+
 function banner() {
  echo "      _          _ _ " 
  echo "     | |        | | |"
@@ -23,25 +25,54 @@ package="$PWD/$(dirname "$0")"
 
 brewbin="brew"
 
-if ! commandExist $brewbin; then
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  checked "Brew is installed"
+function setup() {
+  if ! commandExist $brewbin; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    checked "Brew is installed"
+  fi
+
+  if fileExist "$HOME/.oh-my-zsh/oh-my-zsh.sh"; then
+    checked "OH MyZSH is installed - skip this step"
+  else 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    checked "Installing zsh plugin"
+  fi
+
+  if fileExist $shellConfig; then
+    checked "ZSH configuration already exist - need to backup $shellConfigBackup"
+    cp -f "$shellConfig" "$shellConfigBackum"
+  fi
+
+  cp "$package/files/zshrc" "$shellConfig"
+  checked "ZSH configuration is created $shellConfig"
+
+
+  packagedone "Shell is configure and ready to use."
+}
+
+
+function help() {
+  helptext " "
+  helptext "Description:"
+  helptext "Setup ZSH and install Homebrew."
+  helptext "This include command line aliases, theme, automation and more"
+  helptext " "
+}
+
+function version() {
+  helptext "Package version: $VERSION"
+}
+
+
+if [ "$1" == "--help" ]; then
+  help
+  exit;
 fi
 
-if fileExist "$HOME/.oh-my-zsh/oh-my-zsh.sh"; then
-  checked "OH MyZSH is installed - skip this step"
-else 
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  checked "Installing zsh plugin"
+if [ "$1" == "--version" ]; then
+  version
+  exit;
 fi
 
-if fileExist $shellConfig; then
-  checked "ZSH configuration already exist - need to backup $shellConfigBackup"
-  cp -f "$shellConfig" "$shellConfigBackum"
-fi
+setup
 
-cp "$package/files/zshrc" "$shellConfig"
-checked "ZSH configuration is created $shellConfig"
-
-
-packagedone "Shell is configure and ready to use."
