682889c43b65cb4086a9738bb977821fe9c909c2
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2021-11-29 23:26:56 +0200

    feat(shell): split some of the files into own and remove unused theme
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

---
+++ packages/shell/files/alias.zsh
@@ -0,0 +1,25 @@
+#
+# User configuration
+# New Alias
+alias p="cd ~/Projects"
+alias b="cd ~/blog"
+alias myip='ifconfig | grep "inet " | grep -v 127.0.0.1'
+alias getNodeSize='find . -name "node_modules" -type d -prune -print | xargs du -chs'
+
+alias ll="exa -a -l --no-user --no-time --git -s type --icons"
+alias l="exa -a -s type --icons"
+
+# Usefull stuff for presentation and seeing dotfiles
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias showall='defaults write com.apple.finder AppleShowAllFiles YES && killall Finder'
+alias hideall='defaults write com.apple.finder AppleShowAllFiles NO && killall Finder'
+
+# Get rid of those pesky .DS_Store files recursively
+alias dsclean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
+
+#
+# NeoVide
+#
+# Need to be installed and configure
+alias neovide=~/Github/neovide/target/release/neovide --multigrid
