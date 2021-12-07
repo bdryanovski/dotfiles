664b55eec7ca8df21c52d958d8ef2ec2a6d385ab
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2021-12-08 00:28:40 +0200

    feat(base|git|osx): update packages wit additional settings
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- packages/base/init.sh
+++ packages/base/init.sh
@@ -16,25 +16,67 @@ function banner() {
 
 
 brewbin="brew"
+basersa="~/.ssh/id_rsa"
 
 function setup() {
- if ! commandExist $brewbin; then
+  if ! commandExist $brewbin; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     checked "Homebrew is installed"
+
+    brew update
+    checked "Homebrew is updated"
   else
     skip "Homebrew is already installed on this system"
 
-    checked "Homebrew update itself "
     brew update
+    checked "Homebrew update itself "
 
-    checked "Homebrew update all installed packages"
     brew upgrade
+    checked "Homebrew update all installed packages"
 
-    checked "Homebrew upgrade all casks"
     brew upgrade --cask
+    checked "Homebrew upgrade all casks"
 
-    checked "Homebrew clenup unneeded packages"
+  fi
+
+  
+  if ! isMacOS; then
+    xcode-select --install
+    checked "Xcode basic packages are installed"
+  fi
+
+  if fileExist $basersa; then
+    helptext "Every new machine require to have new SSH key"
+
+    ssh-keygen -t rsa
+
+    helptext "Please go and register this key to Github"
+    helptext "https://github.com/account/ssh"
+    read -p "Press [Enter] to continue ..."
+
+    checked "New SSH Key generated"
+  fi
+
+  helptext "Installing some packages to setup the machine"
+
+  brew install wget 
+
+  checked "wget is installed"
+
+  if ! isMacOS; then 
+    helptext "Install MacOS applications with brew"
+
+    apps=(
+      google-chrome
+      iterm2
+      spotify
+    )
+
+    brew install --cask --appdir="/Applications" ${apps[@]}
+    checked "Brew cask installed few applications ${apps[@]}"
+
     brew cleanup
+    checked "Homebrew clenup packages"
 
   fi
 
