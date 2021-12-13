8c3a4ac459975b8c19401ebd900578b9c88e06b0
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2021-12-13 17:02:00 +0200

    feat(shell): adding you-should-use plugin for zsh - to remind me that I have alias for that
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

--- packages/nodejs/init.sh
+++ packages/nodejs/init.sh
@@ -5,13 +5,13 @@ source ./helpers.sh
 VERSION="1.0.0"
 
 function banner() {
-  echo "  _   _           _           _  _____ " 
+  echo "  _   _           _           _  _____ "
   echo " | \ | |         | |         | |/ ____|"
   echo " |  \| | ___   __| | ___     | | (___  "
   echo ' | . ` |/ _ \ / _` |/ _ \_   | |\___ \ '
   echo " | |\  | (_) | (_| |  __/ |__| |____) |"
   echo " |_| \_|\___/ \__,_|\___|\____/|_____/ "
-  echo "                                       " 
+  echo "                                       "
 }
 
 nvmbin="nvm"
@@ -19,7 +19,6 @@ nvmscript="$HOME/.nvm/nvm.sh"
 nodeVersion="16.13.0"
 
 function setup() {
-
   # This for the most part will always run
   # The reason for that is that nvm as command is comming from bashrc/zshrc configuration on runtime
   # this type of information is not part of the script env, so another check is done to be sure
