c75fe352116e267635e9a4d0a95eec5372ca629c
bdryanovski <bozhidar.dryanovski@theopentag.com>
2024-05-23 09:57:31 +0300

    feat(vim): sync back nvim configuration
    Signed-off-by: bdryanovski <bozhidar.dryanovski@theopentag.com>

--- packages/vim/files/lua/bdryanovski/plugins/autocomplite.lua
+++ packages/vim/files/lua/bdryanovski/plugins/autocomplite.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
-		-- "L3MON4D3/LuaSnip", -- snippet engine
+		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
@@ -23,7 +23,7 @@ return {
 			},
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
-					-- luasnip.lsp_expand(args.body)
+					require("luasnip").lsp_expand(args.body)
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
@@ -33,12 +33,12 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				-- { name = "luasnip" }, -- snippets
+				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 			}),
